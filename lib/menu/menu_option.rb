60abeb2d7a66f62e8ad86b57253c524b6d3af62e
Santiago <Santiago>
2016-09-06 20:08:18 -0300

    Parte 5

--- lib/menu/menu_option.rb
+++ lib/menu/menu_option.rb
@@ -1,4 +1,3 @@
-require_relative '../utils'
 
 class MenuOption
 
@@ -8,7 +7,7 @@ class MenuOption
     @y = y
   end
 
-  def draw(coord_x, coord_y, coord_z, scale_x, scale_y, color)
+  def draw
     @image.draw(@x, @y, 1, 1, 1, Utils::TEXT_COLOR_LIGHT)
   end
 
